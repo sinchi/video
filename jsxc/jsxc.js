8a0f6721d1d1da49ba5edcce30d7d9910efecffa
sinchi <ayoub.belghar@gmail.com>
2017-08-08 23:56:00 +0100

    change turn

--- jsxc/jsxc.js
+++ jsxc/jsxc.js
@@ -8648,7 +8648,7 @@ jsxc.options = {
 
       /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
       iceServers: [{
-         urls: 'turn:numb.viagenie.ca', username: 'ayoub.belghar@gmail.com', password: '3afritto'
+         url: 'turn:ayoub.belghar@gmail.com@numb.viagenie.ca', credential: '3afritto'
       }]
    },
 
