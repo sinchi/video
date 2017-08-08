fed68f31679f13098ec3509a1f34db3907b96d9c
sinchi <ayoub.belghar@gmail.com>
2017-08-08 23:46:29 +0100

    change turn

--- jsxc/jsxc.js
+++ jsxc/jsxc.js
@@ -8648,7 +8648,7 @@ jsxc.options = {
 
       /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
       iceServers: [{
-         urls: 'turn:numb.viagenie.ca:3478', username: 'ayoub.belghar@gmail.com', password: '3afritto'
+         urls: 'numb.viagenie.ca:3478', username: 'ayoub.belghar@gmail.com', password: '3afritto'
       }]
    },
 
