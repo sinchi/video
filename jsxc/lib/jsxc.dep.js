7dc9f2ef7a2236a7827b1520314c255712796d35
sinchi <ayoub.belghar@gmail.com>
2017-08-08 23:10:37 +0100

    change turn

--- jsxc/lib/jsxc.dep.js
+++ jsxc/lib/jsxc.dep.js
@@ -16944,7 +16944,7 @@ function SessionManager(conf) {
     this.config = {
         debug: false,
         peerConnectionConfig: {
-            iceServers: conf.iceServers || [{urls: 'stun:stun.l.google.com:19302'}]
+            iceServers: conf.iceServers || [{urls: 'turn:belghar.hopto.org:3478'}]
         },
         peerConnectionConstraints: {
             optional: [
