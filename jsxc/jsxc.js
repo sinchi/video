25b0763872c8e3a3918f529bb36ee2d0ce3b35cb
sinchi <ayoub.belghar@gmail.com>
2017-08-08 23:59:18 +0100

    change turn

--- jsxc/jsxc.js
+++ jsxc/jsxc.js
@@ -8648,7 +8648,7 @@ jsxc.options = {
 
       /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
       iceServers: [{
-         url: 'turn:ayoub.belghar@gmail.com@numb.viagenie.ca', credential: '3afritto'
+         urls: 'stun:stun.stunprotocol.org'
       }]
    },
 
