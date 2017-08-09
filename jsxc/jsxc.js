c14327328dfb7f901209f1a3fc6242a7d9bbd14c
sinchi <ayoub.belghar@gmail.com>
2017-08-09 23:27:55 +0100

    change turn home

--- jsxc/jsxc.js
+++ jsxc/jsxc.js
@@ -8649,7 +8649,7 @@ jsxc.options = {
       /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
       iceServers: [
         //{"urls":["turn:belghar.hopto.org"],"username":"ayoub","credential":"xmpp"}
-        {"urls":["turn:numb.viagenie.ca"],"username":"ayoub.belghar@gmail.com","credential":"3afritto"}
+        {"url":"turn:test@belghar.hopto.org:3478","credential":"test"}
         //{urls: 'stun:stun.stunprotocol.org'}
       ]
    },
